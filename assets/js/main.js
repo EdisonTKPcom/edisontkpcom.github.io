03a1de2d6dfb6bcd551f2062d8a728b4b610649a
edisontkp <im@edisontkp.com>
2024-10-30 03:39:51 +0000

    update pdf name

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,7 +101,7 @@ let resumeButton = document.getElementById('resume-button')
 /* Html2pdf options */
 let opt = {
   margin:       0,
-  filename:     'mr_raazzput_cv.pdf',
+  filename:     'edisontkp_cv.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
   jsPDF:        { format: 'a4', orientation: 'portrait' }
